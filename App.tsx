9387d7956db747fedca6799df8b9711c8545f8f7
ksamrat224 <ksamrat224@gmail.com>
2025-06-19 15:32:45 +0545

    splash via splash package

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'react-native-magnus';
 import LoginScreen from './src/LoginScreen';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    if(SplashScreen && SplashScreen.hide) {
+    SplashScreen.hide();
+    }
+  }, []);
   return (
     <ThemeProvider>
       <LoginScreen />
