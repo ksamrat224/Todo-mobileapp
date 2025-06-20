f8b74f277b4092445816ee02e3aca0bd91b6e0d5
ksamrat224 <ksamrat224@gmail.com>
2025-06-20 14:50:52 +0545

    Login page created

--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -1,32 +1,40 @@
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { Text, Div, StatusBar } from 'react-native-magnus';
 import CustomInput from './components/CustomInput';
 import CustomButton from './components/CustomButton';
 
 const LoginScreen = () => {
- 
   return (
     <>
-   <StatusBar backgroundColor={'#154360'} barStyle="light-content"/>
-    <SafeAreaView>
-      <Div>
-        <Div>
-          <Text color='white' fontSize="4xl" fontWeight="bold" textAlign="center" mt="5">
-            Login Page
-          </Text>
-          <CustomInput placeholder='Enter your email'/>
-          <CustomInput
-            placeholder='Enter your password'
-            type='password'
-          />
-          <CustomButton content='Log In'/>
+      <StatusBar backgroundColor={'#154360'} barStyle="light-content" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#154360' }}>
+        <Div flex={1} justifyContent="center" alignItems="center" bg="#154360">
+          <Div w={'100%'} px={20} >
+            <Text
+              color="white"
+              fontSize="4xl"
+              fontWeight="bold"
+              textAlign="center"
+              pb="xl"
+            >
+              Login Page
+            </Text>
+            <CustomInput placeholder="Enter your email" />
+            <CustomInput placeholder="Enter your password" type="password" />
+            <CustomButton content="Log In" />
+          </Div>
+          <Div w={'100%'} flexDir='row' alignItems='center' justifyContent='center' pt={10} >
+          <Text>Don't have an account?{" "}</Text>
+          <TouchableOpacity>
+            <Text color="red600" fontWeight="bold" textDecorLine='underline'>
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+          </Div>
         </Div>
-
-      </Div>
-    </SafeAreaView>
+      </SafeAreaView>
     </>
-      
   );
 };
 
