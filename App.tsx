7fb6825cd032f4ad4d24a86150a030e8284cddf2
ksamrat224 <ksamrat224@gmail.com>
2025-06-20 21:09:32 +0545

    navigation in root file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import React, { useEffect } from 'react';
-import { Button, ThemeProvider } from 'react-native-magnus';
-import LoginScreen from './src/LoginScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'react-native-magnus';
 import SplashScreen from 'react-native-splash-screen';
-import CustomInput from './src/components/CustomInput';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginScreen from './src/LoginScreen';
 import SignUp from './src/SignUp';
+import Home from './src/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   useEffect(() => {
-    if(SplashScreen && SplashScreen.hide) {
-    SplashScreen.hide();
+    if (SplashScreen && SplashScreen.hide) {
+      SplashScreen.hide();
     }
   }, []);
+
   return (
     <ThemeProvider>
-      <SignUp />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
