464f9c1d27b28c407ed2e04f2dad6c99a595508f
ksamrat224 <ksamrat224@gmail.com>
2025-06-21 15:30:58 +0545

    logout functionality

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CustomButton from './components/CustomButton';
+import { Div } from 'react-native-magnus';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home laude</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation<any>();
 
-export default Home
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('token');
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Login' }],
+    });
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5' }}>
+      <Text style={{ fontSize: 18, marginBottom: 20 }}>Welcome to Home Screen!</Text>
+      <Div mt="lg" w="80%">
+        <CustomButton content="Logout" onPress={handleLogout} bg="red600" />
+      </Div>
+    </View>
+  );
+};
+
+export default Home;
