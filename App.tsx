2642be3153d25f7f77611b3bee7c0be0ee1ee962
ksamrat224 <ksamrat224@gmail.com>
2025-06-21 15:21:50 +0545

    protected route created

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'react-native-magnus';
 import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -7,21 +7,49 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/LoginScreen';
 import SignUp from './src/SignUp';
 import Home from './src/Home';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { jwtDecode } from 'jwt-decode';
+import { ActivityIndicator, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [initialRoute, setInitialRoute] = useState<string | null>(null);
+
   useEffect(() => {
-    if (SplashScreen && SplashScreen.hide) {
-      SplashScreen.hide();
-    }
+    SplashScreen?.hide?.();
+    const init = async () => {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        try {
+          const decoded: any = jwtDecode(token);
+          const isExpired = decoded.exp * 1000 < Date.now();
+          setInitialRoute(isExpired ? 'Login' : 'Home');
+        } catch (err) {
+          console.error('Token decoding error:', err);
+          setInitialRoute('Login');
+        }
+      } else {
+        setInitialRoute('Login');
+      }
+    };
+    init();
   }, []);
+
+  if (!initialRoute) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#154360" />
+      </View>
+    );
+  }
 
   return (
     <ThemeProvider>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Login"
+          initialRouteName={initialRoute}
           screenOptions={{ headerShown: false }}
         >
           <Stack.Screen name="Login" component={LoginScreen} />
